c492e944eb0e2ae06060ef5b73b9966b81b91a00
ひろろ <hiro_meat_2003@icloud.com>
2025-10-30 15:39:09 +0900

    edit declare DHTesp

---
+++ src/main/DHTHumidity.cpp
@@ -0,0 +1,33 @@
+#include "Sensor.h"
+#include <DHTesp.h>
+
+#define DHT_PIN 5
+DHTesp dht_humi;
+  
+void DHTHumidity::run() {
+  dht_humi.setup(DHT_PIN, DHTesp::DHT11);
+  Serial.println("DHT Humidity Sensor started");
+}
+
+void DHTHumidity::read() {
+  TempAndHumidity d = dht_humi.getTempAndHumidity();
+  if (isnan(d.humidity)) {
+    Serial.println("Failed to read humidity from DHT");
+    return;
+  }
+
+  m_data = String(d.humidity, 1);
+  m_numberOfSensorData++;
+  if (m_numberOfSensorData >= m_maxSize) m_numberOfSensorData = 0;
+
+  // コンテンツ名をICN形式で設定
+  m_contentName = String("/sensor/humid/") + String(m_numberOfSensorData);
+}
+
+String DHTHumidity::getData() {
+  return m_data;
+}
+
+String DHTHumidity::getContentName() {
+  return m_contentName;
+}
