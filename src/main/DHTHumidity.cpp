69b4d607d3b33eb1521a38f6ed6d0bcf83a02516
ひろろ <hiro_meat_2003@icloud.com>
2025-10-31 22:05:26 +0900

    fix : add refactoring

--- src/main/DHTHumidity.cpp
+++ src/main/DHTHumidity.cpp
@@ -1,14 +1,17 @@
 #include "Sensor.h"
 #include <DHT.h>
-  
+
+// DHTの初期化はDHTTemperatureで行う
 void DHTHumidity::run() {
   Serial.println("DHT Temperature And Humidity Sensor started");
 }
 
+// 湿度データ読み込み
 void DHTHumidity::read() {
 
   m_data = dht.readHumidity();
 
+// データのエラーチェック
   if (isnan(m_data)) {
     Serial.println("Failed to read humidity from DHT");
     return;
@@ -21,6 +24,7 @@ void DHTHumidity::read() {
    m_contentName = String("/sensor/humid/") + String(m_numberOfSensorData);*/
 }
 
+// 湿度データ取得
 float DHTHumidity::getData() {
   return m_data;
 }
