097dbd9c6586fb5a750105e056d8439d2444b119
ひろろ <hiro_meat_2003@icloud.com>
2025-11-15 17:12:00 +0900

    delete：不要な行の削除 improve：DHTSensorファイルの追加に伴う互換性の調整

--- src/main/DHTHumidity.cpp
+++ src/main/DHTHumidity.cpp
@@ -1,34 +1,23 @@
 #include "Sensor.h"
 #include <DHT.h>
 
-// DHTの初期化はDHTTemperatureで行う
-void DHTHumidity::run() {
-  Serial.println("DHT Temperature And Humidity Sensor started");
-}
+DHTHumidity::DHTHumidity(DHT& dht_ref)
+  : dhtHumi(dht_ref)
+{}
 
 // 湿度データ読み込み
 void DHTHumidity::read() {
 
-  m_data = dht.readHumidity();
+  m_data = dhtHumi.readHumidity();
 
 // データのエラーチェック
   if (isnan(m_data)) {
     Serial.println("Failed to read humidity from DHT");
     return;
   }
-
-  /*m_numberOfSensorData++;
-  if (m_numberOfSensorData >= m_maxSize) m_numberOfSensorData = 0;
-
-   コンテンツ名をICN形式で設定
-   m_contentName = String("/sensor/humid/") + String(m_numberOfSensorData);*/
 }
 
 // 湿度データ取得
 float DHTHumidity::getData() {
   return m_data;
 }
-
-/*String DHTHumidity::getContentName() {
-  return m_contentName;
-}*/
